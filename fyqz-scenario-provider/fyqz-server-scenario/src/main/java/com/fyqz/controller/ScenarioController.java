3c0d0f9fb6ca9d2280af6d95b5a53e73ee070e0d
1158553513@qq.com <1158553513@qq.com>
2019-06-21 14:17:27 +0800

    初始化推演项目

--- fyqz-scenario-provider/fyqz-server-scenario/src/main/java/com/fyqz/controller/ScenarioController.java
+++ fyqz-scenario-provider/fyqz-server-scenario/src/main/java/com/fyqz/controller/ScenarioController.java
@@ -1,8 +1,6 @@
 package com.fyqz.controller;
 
 
-import com.fyqz.rpc.UserServiceFeign;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,25 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ScenarioController {
 
-    @Autowired
-    private UserServiceFeign userServiceFeign;
-
-   /* *
-     * 向前端提供一个访问地址；通过userServiceFeign调用服务并返回结果
-     * @return
-    @ApiOperation(value = "想定测试获取用户",notes = "想定测试获取用户")
-    @RequestMapping(value = "/personScenario",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
-    public String person(){
-        String result = userServiceFeign.getUser();
-        return result;
-    }
-
-    @ApiOperation(value = "测试获取想定用户",notes = "测试获想定取用户")
-    @RequestMapping(value = "/userScenario",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
-    public String user(){
-        User user = new User();
-        user.setName("I am  From userScenario");
-        String result = userServiceFeign.hello(user);
-        return result;
-    }*/
 }
