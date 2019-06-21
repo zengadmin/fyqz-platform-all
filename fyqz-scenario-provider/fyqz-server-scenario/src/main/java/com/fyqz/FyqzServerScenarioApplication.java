48dc048f1329dc896886133a69203e8caf3bf007
1158553513@qq.com <1158553513@qq.com>
2019-06-21 16:42:20 +0800

    删除idea

--- fyqz-scenario-provider/fyqz-server-scenario/src/main/java/com/fyqz/FyqzServerScenarioApplication.java
+++ fyqz-scenario-provider/fyqz-server-scenario/src/main/java/com/fyqz/FyqzServerScenarioApplication.java
@@ -21,7 +21,8 @@ public class FyqzServerScenarioApplication {
 		System.out.println("请在控制台指定fyqz-service-scenario应用的端口号 —— [端口号随意指定，注意不要与本机端口号出现冲突即可]");
 		Scanner scanner = new Scanner(System.in);
 		String port = scanner.nextLine();
-		new SpringApplicationBuilder(FyqzServerScenarioApplication.class).properties("server.port=" + port).run(args);//启动项目
+		//启动项目
+		new SpringApplicationBuilder(FyqzServerScenarioApplication.class).properties("server.port=" + port).run(args);
 		System.out.println("================================================== 应用fyqz-service-scenario 启动成功 =============================================================");
 	}
 }
