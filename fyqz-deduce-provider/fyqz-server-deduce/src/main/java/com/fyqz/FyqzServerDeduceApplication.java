9c1640788b7a4586aaa4046130787f9cf2b9eb27
1158553513@qq.com <1158553513@qq.com>
2019-06-21 14:27:13 +0800

    初始化推演项目

---
+++ fyqz-deduce-provider/fyqz-server-deduce/src/main/java/com/fyqz/FyqzServerDeduceApplication.java
@@ -0,0 +1,31 @@
+package com.fyqz;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.Scanner;
+
+/**
+ * @author zengchao
+ */
+@EnableSwagger2Doc
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+public class FyqzServerDeduceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("================================================== 开始启动 应用fyqz-server-deduce =============================================================");
+		System.out.println("请在控制台指定fyqz-server-deduce应用的端口号 —— [端口号随意指定，注意不要与本机端口号出现冲突即可]");
+		Scanner scanner = new Scanner(System.in);
+		String port = scanner.nextLine();
+		//启动项目
+		new SpringApplicationBuilder(FyqzServerDeduceApplication.class).properties("server.port=" + port).run(args);
+		System.out.println("================================================== 应用fyqz-service-scenario 启动成功 =============================================================");
+	}
+}
